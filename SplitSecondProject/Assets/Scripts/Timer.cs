350bf527afdde0253dd61d45a4500af757ffd9a6
Maximilian Slabschi <m.slabschi@students.htl-leonding.ac.at>
2025-01-13 12:12:48 +0100

    added Timer

---
+++ SplitSecondProject/Assets/Scripts/Timer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    float elapsedTime;
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        int milliseconds = Mathf.FloorToInt((elapsedTime % 1) * 1000);
+        timerText.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
+    }
+}
